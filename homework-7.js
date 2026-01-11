ee1e7a56598c19dac3b3df4a867de307a071cd80
Акмаль Бегметов <akmabeat25@gmail.com>
2026-01-11 22:09:25 +0600

    fixed some errors

--- homework-7.js
+++ homework-7.js
@@ -1,3 +1,5 @@
+import { socialMediaComments } from './comments.js';
+
 // 2. Создать массив чисел от 1 до 10. Отфильтровать его таким образом, чтобы мы получили массив чисел, начиная с 5
 const arrayNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log(arrayNumbers);
@@ -8,7 +10,7 @@ console.log(newArrayNumbers);
 // 3. Создать массив строк, относящихся к любой сущности (название фильмов/книг, кухонные приборы, мебель и т.д.), проверить, есть ли в массиве какая-то определенная сущность
 const fruitArray = ['apple', 'banana', 'cherry', 'grape'];
 const searchFruit = 'apple';
-const isExist = fruitArray.some(fruit => fruit === searchFruit);
+const isExist = fruitArray.includes(searchFruit);
 console.log(isExist);
 
 // 4. Написать функцию, которая аргументом будет принимать массив и изменять его порядок на противоположный ("переворачивать"). Два вышеуказанных массива с помощью этой функции перевернуть
@@ -19,36 +21,29 @@ console.log(reverseArray(arrayNumbers));
 console.log(reverseArray(fruitArray));
 
 // 5.
-import { socialMediaComments } from './comments.js';
 
 const filteredComments = socialMediaComments.filter(comment => comment.email.endsWith('.com'));
 console.log(filteredComments);
 
 // 8. Перебрать массив таким образом, чтобы пользователи с id меньше или равно 5 имели postId: 2, а те, у кого id больше 5, имели postId: 1
-const updatedComments = socialMediaComments.map(comment => {
-  return {
-    ...comment,
-    postId: comment.id <= 5 ? 2 : 1
-  };
-});
+const updatedComments = socialMediaComments.map(comment => ({
+  ...comment,
+  postId: comment.id <= 5 ? 2 : 1
+}));
 console.log(updatedComments);
 
 // 9. Перебрать массив, чтобы объекты состояли только из айди и имени
-const idAndName = socialMediaComments.map(comment => {
-  return {
-    id: comment.id,
-    name: comment.name
-  };
-});
+const idAndName = socialMediaComments.map(comment => ({
+  id: comment.id,
+  name: comment.name
+}));
 console.log(idAndName);
 
 // 10. Перебираем массив, добавляем объектам свойство isInvalid и проверяем: если длина тела сообщения (body) больше 180 символов - устанавливаем true, иначе false
-const checkedBodyLength = socialMediaComments.map(comment => {
-  return {
-    ...comment,
-    isInvalid: comment.body.length > 180
-  };
-});
+const checkedBodyLength = socialMediaComments.map(comment => ({
+  ...comment,
+  isInvalid: comment.body.length > 180
+}));
 console.log(checkedBodyLength);
 
 // 11. Используем reduce для получения массива почт
